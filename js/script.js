78f563f66a4d5b91977e577db79e2cbb422433bb
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-06-20 19:51:12 +0200

    Finish V1

--- js/script.js
+++ js/script.js
@@ -1,40 +1,100 @@
+// SEARCH COMPONENT load function
+function searchComponent() {
+  const searchComponent = document.querySelector('#search-component').innerHTML;
+  Mustache.parse(searchComponent);
+
+  const rendered = Mustache.render(searchComponent, { someVar: 'some value' });
+  document.querySelector('#render-box').innerHTML = rendered;
+}
+
+// CONTRY COMPONENT load function
+function countryComponent(countryName = 'test') {
+  const countryComponent = document.querySelector('#country-component')
+    .innerHTML;
+  Mustache.parse(countryComponent);
+
+  getCountry(countryName).then(function(data) {
+    const { name, flag, timezones, population, capital, region } = data[0];
+    console.log(name);
+    console.log(flag);
+    console.log(timezones);
+    console.log(population);
+    console.log(capital);
+
+    const rendered = Mustache.render(countryComponent, {
+      name: countryName,
+      flag: flag,
+      capital: capital,
+      population: population,
+      region: region,
+      timezones: timezones,
+      wiki: `https://en.wikipedia.org/wiki/${name}`
+    });
+
+    document.querySelector('#render-box').innerHTML = rendered;
+  });
+}
+
+// GET COUNTRY
+
+function getCountry(param) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${param}`)
+    .then(res => res.json())
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   const url = window.location.href;
-  console.log(url);
   const urlParam = new URL(url);
-  console.log(urlParam.searchParams.get('a'));
+  if (urlParam.searchParams.has('country')) {
+    countryComponent(urlParam.searchParams.get('country'));
+  } else {
+    searchComponent();
+    document.querySelector('#input').addEventListener('input', function(e) {
+      console.log(e.target.value);
+      document.querySelector('#output').innerHTML = '';
 
-  document.querySelector('#input').addEventListener('input', function(e) {
-    console.log(e.target.value);
-    document.querySelector('#output').innerHTML = '';
-
-    if (e.target.value === '') {
-      console.log('type in the country');
-    } else {
-      fetch(`https://restcountries.eu/rest/v1/name/${e.target.value}`)
-        .then(res => res.json())
-        .then(data => {
-          console.log(data);
-          document.querySelector('#input').innerHTML = '';
-          const countries = [];
-          data.forEach(function(x) {
-            countries.push(x.name);
+      if (e.target.value === '') {
+        console.log('No country typed in');
+        document.querySelector('#output').innerHTML =
+          'Type in the country name';
+      } else {
+        getCountry(e.target.value)
+          .then(data => {
+            document.querySelector('#input').innerHTML = '';
+            const countries = [];
+            data.forEach(function(x) {
+              countries.push(x.name);
+            });
+            let allC = document.createElement('ul');
+            allC.setAttribute('class', 'list-group');
+            countries.forEach(x => {
+              let element = document.createElement('li');
+              element.setAttribute('class', 'list-group-item');
+              let link = document.createElement('a');
+              link.setAttribute('href', `?country=${x}`);
+              link.innerText = 'More info';
+              let par = document.createElement('p');
+              par.innerHTML = x;
+              element.appendChild(par);
+              element.appendChild(link);
+              allC.appendChild(element);
+            });
+            document.querySelector('#output').innerHTML = '';
+            document.querySelector('#output').appendChild(allC);
+          })
+          .catch(err => {
+            console.log(err);
+            document.querySelector('#output').innerHTML = 'No such country';
           });
-          console.log(countries);
-          let allC = document.createElement('div');
-          countries.forEach(x => {
-            let element = document.createElement('div');
-            element.innerText = x;
-            allC.appendChild(element);
-          });
-          document.querySelector('#output').innerHTML = '';
-          document.querySelector('#output').appendChild(allC);
-        })
-        .catch(err => {
-          console.log(err);
-          document.querySelector('#output').innerHTML = 'No such country';
-        });
-      document.querySelector('#output').innerHTML = 'Loading';
-    }
-  });
+        document.querySelector(
+          '#output'
+        ).innerHTML = `<div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>`;
+      }
+    });
+  }
 });
